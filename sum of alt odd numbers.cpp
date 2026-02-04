0f8f145cd36628242822dda741b4642f4481e039
gaurinandan <pranavrajkolkar7@gmail.com>
2026-02-04 09:05:53 +0530

    Add files via upload

---
+++ sum of alt odd numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the number of elements\n";
+	cin>>n;
+	int a[n];
+	cout<<"Enter the elements\n";
+	int sum=0;
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+		if(i%2!=0)
+			sum+=a[i];
+	}
+	cout<<"sum of elements="<<sum;
+	return 0;
+}
